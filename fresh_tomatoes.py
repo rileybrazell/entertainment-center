94605e45e92e9a49e4de6d90c0def7321a746f83
Riley <rileybrazell@gmail.com>
2017-03-02 19:04:56 -0800

    Supply movie information through TheMovieDB API access

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -125,7 +125,6 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
-    <p>{movie_rating}</p>
 </div>
 '''
 
@@ -144,10 +143,9 @@ def create_movie_tiles_content(movies):
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
-            movie_rating=movie.rating,
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=movie.trailer_youtube_url
         )
     return content
 
